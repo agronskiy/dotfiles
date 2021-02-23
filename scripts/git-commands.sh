c2825a0e17e956269042bc3e7afebae4375c8c04
Alexey Gronskiy <alexey@gronskiy.com>
2021-02-23 21:05:06 +0100

    Added zsh git functions

--- scripts/git-commands.sh
+++ scripts/git-commands.sh
@@ -1,11 +1,13 @@
+# Utility functions for git
+
 function gitrmbranch {
     git branch -D $1
     git push --delete origin $1
 }
-__git_branch_names gitrmbranch
+compdef __git_branch_names gitrmbranch
 
 function gitnewbranch {
     git checkout -b $1
     git push -u origin HEAD
 }
-__git_branch_names gitnewbranch
+compdef __git_branch_names gitnewbranch
