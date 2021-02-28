e6948e5b466bb030d666fc643c31ef1d01fc473c
Alexey Gronskiy <alexey@gronskiy.com>
2021-02-28 06:49:47 +0100

    Explicit file load
    Karabiner refactor
    
    Correcting bootstrapping: user input from tty

--- karabiner/alias.zsh
+++
@@ -1,2 +0,0 @@
-alias karabiner-install-config-to="cp ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json"
-alias karabiner-copy-config-back="cp ~/.config/karabiner/karabiner.json ~/.dotfiles/karabiner/karabiner.json"
