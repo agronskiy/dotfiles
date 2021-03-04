88827fa7e7cb0db82ce5f167582cbe76182ddca3
Alexey Gronskiy <alexey@gronskiy.com>
2021-03-04 21:11:45 +0100

    Corrected loading of fzf preview

--- zsh/path.pre-load.zsh
+++ zsh/path.pre-load.zsh
@@ -1,3 +1,6 @@
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export PATH="$PATH:$HOME/.rvm/bin"
+
+# FZF options
+export FZF_DEFAULT_OPTS='--height 70% --reverse --border --bind=F2:toggle-preview'
