767de80a5cca0db68a1648699e1d0c1b3ce23578
Alexey Gronskiy <alexey@gronskiy.com>
2021-03-05 14:44:13 +0100

    Disabled exit on ctrl-d

---
+++ tmux/aliases.zsh
@@ -0,0 +1,7 @@
+# Should allow to delete the "dummy session" created when not attaching to tmux
+# Credit to https://tech.serhatteker.com/post/2019-11/restore-tmux-after-reboot/
+alias mux="pgrep -vxq tmux && \
+            tmux new -d -s delete-me && \
+            tmux run-shell $HOME/.tmux/plugins/tmux-resurrect/scripts/restore.sh && \
+            tmux kill-session -t delete-me \
+            && tmux attach || tmux attach"
