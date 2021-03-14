b265bfeadc781bec3a51f22c1706b6910f8d0f6c
Alexey Gronskiy <alexey@gronskiy.com>
2021-03-14 21:24:04 +0100

    Completions installation - fix

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,20 +2,23 @@
 
 source $DOTFILES/zsh/logging.explicit-load.zsh
 
-if ! [ -x "$(command -v fzf)" ]
-then
-    log_info "Installing fzf"
-    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf" && "$HOME/.fzf/install"
-    return_val=$?
+install_fzf () {
+    if ! [ -x "$(command -v fzf)" ]
+    then
+        log_info "Installing fzf"
+        git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf" && "$HOME/.fzf/install"
+        return_val=$?
 
-    if [ $return_val -ne 0 ]; then
-        log_fail "Failed to install fzf"
+        if [ $return_val -ne 0 ]; then
+            log_fail "Failed to install fzf"
+        else
+            log_success "Installed fzf"
+        fi
     else
-        log_success "Installed fzf"
+        log_info "Skipped installing fzf"
     fi
-else
-    log_info "Skipped installing fzf"
-fi
+}
+install_fzf
 
 # zsh completions
 install_zsh_completions() {
@@ -36,6 +39,7 @@ install_zsh_completions() {
         )
     fi
 }
+install_zsh_completions
 
 # Conda completions, see https://github.com/esc/conda-zsh-completion/blob/master/_conda
 install_conda_completions() {
