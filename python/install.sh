b92347898e338f8cf07408845387eaeadae976ef
Alexey Gronskiy <alex.gronskiy@snyk.io>
2021-09-29 15:11:12 +0200

    feat(mac/conda): changing miniconda installation to the non-brew one)

--- python/install.sh
+++ python/install.sh
@@ -28,7 +28,19 @@ install_conda() {
         )
     elif [ "$(uname -s)" = "Darwin" ]
     then
-        brew install miniconda
+        (
+            echo "Installing miniconda (Darwin)"
+            cd /tmp
+
+            if [ -f "Miniconda3-latest-MacOSX-x86_64.sh" ]; then
+                rm Miniconda3-latest-MacOSX-x86_64.sh
+            fi
+            curl -LO  https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+            chmod +x Miniconda3-latest-MacOSX-x86_64.sh
+            # `yes` will answer to the licnese agreement.
+            yes | ./Miniconda3-latest-MacOSX-x86_64.sh -b
+            rm Miniconda3-latest-MacOSX-x86_64.sh
+        )
     fi
 
     return_val=$?
