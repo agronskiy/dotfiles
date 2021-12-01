eae5cf596750c0c173d0b94a6af0d871b3ce6fd2
Alexey Gronskiy <alex.gronskiy@snyk.io>
2021-12-01 11:58:08 +0100

    feat(aliases): better aliases for ll/la

--- zsh/aliases.pre-load.zsh
+++ zsh/aliases.pre-load.zsh
@@ -53,12 +53,13 @@ function __tree() {
     # piping.
     if [ -t 1 ]
     then
-        exa -alTF -L 2 --color=always "$@" | less
+        exa -lTF -L 2 --color=always "$@" | less
     else
-        exa -alTF -L 2 "$@"
+        exa -lTF -L 2 "$@"
     fi
 }
 alias trel="__tree"
+alias trela="trel -a"
 
 export EXA_COLORS="xa=37:su=37:sf=37:ur=37:uw=37:ux=37:ue=37:gr=37:gw=37:gx=37:tr=37:tw=37:tx=37:uu=33:un=33:da=36"
 
