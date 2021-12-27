ec6b3c88b1358eb34bee80cd754831551f54023e
Alexey Gronskiy <alex.gronskiy@snyk.io>
2021-12-27 21:27:26 +0100

    feat: adds local directories

--- base-installs/all.yasli.pre.sh
+++ base-installs/all.yasli.pre.sh
@@ -5,3 +5,11 @@ exists_local_bin_dir() {
     [ -d "$HOME/.local/bin"]
 }
 install_wrapper "local bin directory" install_local_bin_dir exists_local_bin_dir
+
+install_home_bin_dir() {
+    mkdir -p "$HOME/bin"
+}
+exists_home_bin_dir() {
+    [ -d "$HOME/bin"]
+}
+install_wrapper "home bin directory" install_home_bin_dir exists_home_bin_dir
