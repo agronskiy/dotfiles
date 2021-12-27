cf4af247fe3634dcc7f94eb1c4d8926d168d0256
Alexey Gronskiy <alex.gronskiy@snyk.io>
2021-12-27 21:31:18 +0100

    fix: bash closing bracket missing

--- base-installs/all.yasli.pre.sh
+++ base-installs/all.yasli.pre.sh
@@ -2,7 +2,7 @@ install_local_bin_dir() {
     mkdir -p "$HOME/.local/bin"
 }
 exists_local_bin_dir() {
-    [ -d "$HOME/.local/bin"]
+    [ -d "$HOME/.local/bin" ]
 }
 install_wrapper "local bin directory" install_local_bin_dir exists_local_bin_dir
 
@@ -10,6 +10,6 @@ install_home_bin_dir() {
     mkdir -p "$HOME/bin"
 }
 exists_home_bin_dir() {
-    [ -d "$HOME/bin"]
+    [ -d "$HOME/bin" ]
 }
 install_wrapper "home bin directory" install_home_bin_dir exists_home_bin_dir
