f61f1ef3808101740223a74b6870fc8de06adbf0
Alexey Gronskiy <alex.gronskiy@snyk.io>
2021-12-28 16:02:45 +0100

    fix: installation

--- base-installs/all.linux.yasli.pre.sh
+++ base-installs/all.linux.yasli.pre.sh
@@ -12,17 +12,10 @@ else
     log_success "Skipped curl"
 fi
 
-if ! [ -x "$(command -v brew)" ]
-then
-    log_info "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 2>&1 |
-        log_cmd
-
-    if [ $? -eq 0 ] ; then
-        log_success "Successfully installed brew"
-    else
-        log_fail "Failed to install brew"
-    fi
-else
-    log_success "Skipped brew"
-fi
+install_build_essential() {
+    $SUDO_CMD apt-get -y install build-essential
+}
+exists_build_essential() {
+    dpkg-query -W -f='${Status}' build-essential | grep "ok installed"
+}
+install_wrapper "build-essentials" install_build_essential exists_build_essential
