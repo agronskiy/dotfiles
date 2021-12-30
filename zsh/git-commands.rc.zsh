c42dc586a3dd6e8ff1649d7a514a56398dba3926
Alexey Gronskiy <alex.gronskiy@snyk.io>
2021-12-30 12:50:57 +0100

    feat: some git corrections - 2

--- zsh/git-commands.rc.zsh
+++ zsh/git-commands.rc.zsh
@@ -1,7 +1,32 @@
 # Based on https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236s
 
+__RESTORE=$(echo -en '\033[0m')
+__RED=$(echo -en '\033[00;31m')
+__GREEN=$(echo -en '\033[00;32m')
+
 is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
+}
+
+_relative_path_helper() {
+  local modifier file_path
+  local red light_red green restore
+
+  while IFS= read -r line
+  do
+      # Lines are in the format `M some/path/to/file` or `A other/file`
+      read -r modifier file_path <<< "$line"
+      case $modifier in
+        [DM])
+          modifier="${__RED}${modifier}${__RESTORE}"
+          ;;
+        A)
+          modifier="${__GREEN}${modifier}${__RESTORE}"
+          ;;
+      esac
+      file_path="$(realpath --relative-to=. $(git rev-parse --show-toplevel)/$file_path)"
+      printf "%s %s\n" "$modifier" "$file_path"
+  done
 }
 
 _git-files-fuzzy() {
@@ -18,9 +43,7 @@ _git-files-fuzzy() {
     else
       commit_plus="$2"
     fi
-    status_command=$(git diff --name-status $commit_minus $commit_plus |
-      awk '{print $2}' |
-      xargs -I '{}' realpath --relative-to=. $(git rev-parse --show-toplevel)/'{}')
+    status_command=$(git diff --name-status $commit_minus $commit_plus | _relative_path_helper)
     filter_command=(tr '\t' ' ')
   fi
   preview_command="git diff $commit_minus $commit_plus "'{-1} | delta -n '"${DELTA_DEFAULT_OPTS:-}"
@@ -28,9 +51,9 @@ _git-files-fuzzy() {
 
   printf "%s\n" "${status_command[@]}" |
   $filter_command |
-    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height 90%" \
-  fzf-tmux $FZF_TMUX_OPTS -p 95%,90% -- -m --ansi --nth 2.. \
-    --preview-window wrap:right:75% \
+    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height 95%" \
+  fzf-tmux $FZF_TMUX_OPTS -p 95%,95% -- -m --ansi --nth 2.. \
+    --preview-window wrap:up:70% \
     --preview "$preview_command" |
   awk '{print $2}' | sed 's/.* -> //'
 
