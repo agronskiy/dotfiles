4ed7636c34ae5579d4b944a07d1f14f5940d853d
Alex Gronskiy <alex.gronskiy@snyk.io>
2022-04-15 20:39:23 +0000

    feat(vscode): rgf runs from integrated terminal

--- fzf/parameters.rc.zsh
+++ fzf/parameters.rc.zsh
@@ -43,7 +43,7 @@ $word'
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -TF -L=2 --color=always $realpath'
 
 # preview on cat/bat/vim/nvim (remember that here cat is aliased to bat)
-zstyle ':fzf-tab:complete:(bat|cat|vim|nvim):*' fzf-preview 'bat --color=always --plain $realpath'
+zstyle ':fzf-tab:complete:(bat|cat|vim|nvim|code):*' fzf-preview 'bat --color=always --plain $realpath'
 
 # NOTE: uncomment below to exclude directories from autocompletion. Usefulness is questionable
 # because we can still want to navigate to the subdirectory.
@@ -77,7 +77,7 @@ _fzf_comprun() {
     cd)           $(__fzfcmd) "$@" --preview 'exa -TF -L=2 --color=always {} | head -200' ;;
     export|unset) $(__fzfcmd) "$@" --preview "eval 'echo \$'{}" ;;
     ssh)          $(__fzfcmd) "$@" --preview 'dig {}' ;;
-    cat|bat|vim|nvim)  $(__fzfcmd) "$@" --preview 'bat --color=always --plain {}' ;;
+    cat|bat|vim|nvim|code)  $(__fzfcmd) "$@" --preview 'bat --color=always --plain {}' ;;
     *)            $(__fzfcmd) "$@" ;;
   esac
 }
